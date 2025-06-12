6a50ab6bbc6eb837d2decd4cc8e080856470b93b
Anish Timsina <anishtimsina899@gmail.com>
2025-06-12 19:20:34 +0545

    initial

--- nodehealth.sh
+++ nodehealth.sh
@@ -12,8 +12,16 @@
 ####################################################
 
 set -x #debug mode
+set -e #exit the script when there is error
+set -o pipefail
+#instead of above 3 commands set -x,-e,-o pipefail we can simply write below command
+
+#set -exo pipefail 
+#but it is better to use above 3 commands separtately as it will be easy to understand
+
 
 df -h # disk usage information, and -h makes the output human-readable.
 free -g #provides system memory (RAM) usage information. 
 nproc  #nproc shows the number of CPU cores.
 ps -ef | grep ubuntu | awk -F " " '{print $2}'
+
